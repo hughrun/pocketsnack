33671beead0555afea85810bbc6e3efc9d1b945a
Hugh Rundle <github@hughr.me>
2018-12-31 22:04:32 +1100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+frequency = # daily, weekly, workdays, fortnightly, monthly
+hour_to_run = 5 # what time of the day to run the script in 24 hour time e.g. this is 5am, 5pm would be 17
+timezone = # tbc how to do this, needed to determine workdays and time of day
+items_per_cycle = 10
+archive_tag = 'TBR' # apply this tag before archiving items
+ignore_tag = 'keep' # don't archive items with this tag
+minimum_videos = None # if you change this to an integer it will be taken from items_per_cycle
+minimum_images = None # if you change this to an integer it will be taken from items_per_cycle
