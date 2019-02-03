ea8e201fb0d53f67a845a854e09e8ab9d02a1a2c
Hugh Rundle <hugh@hughrundle.net>
2019-02-03 16:11:46 +1100

    add daily scheduling and local log filepaths

--- install.sh
+++ install.sh
@@ -18,11 +18,71 @@ if [ $(which python3) ]; then
         sleep 5s
         pocketsnack authorise
           # here we should ask if they want to set up a regular job
-        echo 'Do you want to automatically refresh your Pocket List? This will run "stash" and then "lucky_dip" once per day.'
-        select daily in Yes No
+        echo 'Do you want to automatically refresh your Pocket List? This will run "stash" and then "lucky_dip" once per day, or once per week.'
+        auto=("No Thanks" "Yes Daily" "Yes Weekly")
+        select automated in "${auto[@]}"
         do
-          case $daily in
-            Yes)
+          case $REPLY in
+            3)
+              # which day do they want to refresh their list? Must be between 1 (Monday) and 7 (Sunday)
+              echo "Which day do you want to refresh your Pocket list?"
+              select day in Monday Tuesday Wednesday Thursday Friday Saturday Sunday;
+              do
+                if [[ -z $day ]]; then
+                  echo "Please enter a number between 1 and 7."
+                else
+                  daynum=$REPLY
+                  # what hour do they want to refresh their list? Must be a number between 0 and 23. Is 6 by default.
+                  echo 'Enter an hour between 0 and 23 for when you want to run your script.'
+                  read hour            
+                  isnum='^2[0-3]$|^1?[0-9]$'
+                  while ! [[ $hour =~ $isnum ]]
+                  do
+                    echo 'Invalid choice - please enter a whole number between 0 and 23'
+                    read hour
+                  done
+                  echo "You chose $hour"
+                  # are they running MacOS or Linux
+                  echo "Ok last question: are you running (1) MacOS or (2) Linux?"
+                  select operating_system in MacOS Linux;
+                  do
+                    case $operating_system in
+                      MacOS)
+                        # create a new plist file with the hour number and put it in the right place
+                        sed "26 s/[0-9][0-9]*/$hour/" <daily.plist >~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                        # edit the file in place to update the day
+                        sed -i '' -e "28 s/[0-9][0-9]*/$daynum/" ~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                        # edit the file in place to update the logfiles
+                        logpath="<string>$(pwd)/pocketsnack.log</string>"
+                        errorpath="<string>$(pwd)/pocketsnack-error.log</string>"
+                        sed -i '' -e "13c\\
+                        $logpath
+                        " ~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                        sed -i '' -e "15c\\
+                        $errorpath
+                        " ~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                        # unload launchd file in case we're re-loading it
+                        launchctl unload ~/Library/Launchagents/com.getpocket.pocketsnack.plist
+                        # load launchd file
+                        launchctl load ~/Library/Launchagents/com.getpocket.pocketsnack.plist
+                        echo "You're all set up! Enjoy your new Pocket experience."
+                        exit 0
+                        ;;
+                      Linux)
+                        # edit crontab
+                        (crontab -l ; echo "0 $hour * * $day /usr/local/bin/pocketsnack refresh") 2> /dev/null | sort | uniq | crontab -
+                        echo "You're all set up! Enjoy your new Pocket experience."
+                        exit 0
+                        ;;
+                      *)
+                        echo "Please select 1 for MacOS or 2 for Linux"
+                        ;;
+                    esac
+                  done
+                fi
+              done  
+              ;;
+            2)
               # what hour do they want to refresh their list? Must be a number between 0 and 23. Is 6 by default.
               echo 'Enter an hour between 0 and 23 for when you want to run your script.'
               read hour            
@@ -32,7 +92,7 @@ if [ $(which python3) ]; then
                 echo 'Invalid choice - please enter a whole number between 0 and 23'
                 read hour
               done
-              echo "you chose $hour"
+              echo "You chose $hour"
               # are they running MacOS or Linux
               echo "Ok last question: are you running (1) MacOS or (2) Linux?"
               select operating_system in MacOS Linux
@@ -41,6 +101,17 @@ if [ $(which python3) ]; then
                   MacOS)
                     # create a new plist file with the hour number and put it in the right place
                     sed "22 s/[0-9][0-9]*/$hour/" <hourly.plist >~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                    # edit the file in place to update the logfiles
+                    logpath="<string>$(pwd)/pocketsnack.log</string>"
+                    errorpath="<string>$(pwd)/pocketsnack-error.log</string>"
+                    sed -i '' -e "13c\\
+                    $logpath
+                    " ~/Library/LaunchAgents/com.getpocket.pocketsnack.plist
+                    sed -i '' -e "15c\\
+                    $errorpath
+                    " ~/Library/LaunchAgents/com.getpocket.pocketsnack.plist                    
+                    # unload launchd file in case we're re-loading it
+                    launchctl unload ~/Library/Launchagents/com.getpocket.pocketsnack.plist
                     # load launchd file
                     launchctl load ~/Library/Launchagents/com.getpocket.pocketsnack.plist
                     echo "You're all set up! Enjoy your new Pocket experience."
@@ -58,12 +129,12 @@ if [ $(which python3) ]; then
                 esac
               done
               ;;
-            No)
-              echo 'No script scheduled. You can use pocket-snack manually at any time using the pocketsnack command.'
+            1)
+              echo 'No script scheduled. You can use pocket-snack manually at any time using the `pocketsnack` command.'
               exit 0
               ;;
             *)
-              echo 'Please select 1 to schedule daily refreshes or 2 to exit.'
+              echo 'Please select 1 to exit, 2 to schedule daily refreshes or 3 to schedule weekly refreshes.'
               ;;
           esac
         done
