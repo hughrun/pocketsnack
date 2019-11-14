740f5b9e608bae8aec7459de97e66c563eb11d05
Hugh Rundle <hugh@hughrundle.net>
2019-11-14 19:21:41 +1100

    rearrange archive and list commands to be info tbr and info list respectively

--- main.py
+++ main.py
@@ -64,19 +64,16 @@ parser = ArgumentParser(description='pocketsnack: a command line tool for declut
 subparsers = parser.add_subparsers(help='for more help on these options use [command] --help (e.g. purge --help)')
 
 parser.add_argument(
-    "-a", "--archive", action="store_true", help="return data about items tagged 'tbr' in user's Pocket archive"
-)
-parser.add_argument(
     "-u", "--authorise", action="store_true", help="authorise app to connect to a Pocket account"
-)
-parser.add_argument(
-    "-l", "--list", action="store_true", help="return data about items in user's Pocket list"
 )
 parser.add_argument(
     "-d", "--lucky_dip", action="store_true", help="move random items tagged 'tbr' from archive to list, depending on settings"
 )
 parser.add_argument(
     "-r", "--refresh", action="store_true", help="run 'stash' and then 'lucky_dip' in one operation"
+)
+parser.add_argument(
+    "-t", "--test", action="store_true", help="test whether API call returns data"
 )
 
 # stash
@@ -86,9 +83,18 @@ parser.add_argument(
 parser.add_argument(
     "-s", "--stash", action="store_true", help="add 'tbr' tag to all items in user list and archive them, with exceptions as per settings"
 )
-parser.add_argument(
-    "-t", "--test", action="store_true", help="test whether API call returns data"
+
+# SUBPARSERS
+
+# new 'info' subparser to reduce confusion between v1 option 'archive' and 'stash'
+info = subparsers.add_parser(
+  'info', help='get information on contents of Pocket account'
 )
+
+info.add_argument(
+    'info_choice', default='list', const="list", nargs='?', choices=('list', 'tbr'), help="return data about items in user's Pocket list or items tagged 'tbr' in users's Pocket archive. For example \033[1;36mpocketsnack info tbr\033[1;m. Defaults to 'list'"
+)
+
 # purge
 purge_choice = subparsers.add_parser(
   "purge", help="remove all tags from list, archive, or both ('all'), depending on the second argument provided and excepting tags listed in 'retain_tags' in settings.py. Defaults to 'list' if no argument is provided"
@@ -101,76 +107,80 @@ options = parser.parse_args()
 
 if __name__ == '__main__':
 
-  if options.archive:
-    # Retrieve info about the user's list
-    response = pt.get_tbr(consumer_key, settings.pocket_access_token, archive_tag)
-    items = response['list']
-    longreads = 0
-    for item in items:
-      # is it a long read?
-      if 'word_count' in items[item]:
-        words = int(items[item]['word_count'])
-        longread = True if  words > settings.longreads_wordcount else False
-      else:
-        longread = False
-      if longread:
-        longreads += 1
-    print('The TBR archive has ' + str(len(response['list'])) + ' items and ' + str(longreads) + ' are longreads.')
+  # print(options)
 
   if options.authorise:
     # Run authorise once first to retrieve a pocket_access_token
     auth = pt.authorise(consumer_key, redirect_uri)
     print(auth)
 
-  if options.lucky_dip:
+  elif options.lucky_dip:
     print('\033[0;36mRunning lucky dip...\033[0;m')
     dip = pt.lucky_dip(consumer_key, settings.pocket_access_token, settings.archive_tag, settings.items_per_cycle, settings.num_videos, settings.num_images, settings.num_longreads, settings.longreads_wordcount)
     print('\033[0;36m' + dip + '\033[0;m')
 
-  if options.list:
-    # Retrieve info about the user's list
-    response = pt.get_list(consumer_key, settings.pocket_access_token)
-    items = response['list']
-    longreads = 0
-    for item in items:
-      # is it a long read?
-      if 'word_count' in items[item]:
-        words = int(items[item]['word_count'])
-        longread = True if  words > settings.longreads_wordcount else False
-      else:
-        longread = False
-      if longread:
-        longreads += 1
-    print('The user list has ' + str(len(response['list'])) + ' items and ' + str(longreads) + ' are longreads.')
+  elif hasattr(options, 'info_choice'):
+
+    if options.info_choice == 'tbr':
+      # Retrieve info about the user's list
+      response = pt.get_tbr(consumer_key, settings.pocket_access_token, archive_tag)
+      items = response['list']
+      longreads = 0
+      for item in items:
+        # is it a long read?
+        if 'word_count' in items[item]:
+          words = int(items[item]['word_count'])
+          longread = True if  words > settings.longreads_wordcount else False
+        else:
+          longread = False
+        if longread:
+          longreads += 1
+      print('The TBR archive has ' + str(len(response['list'])) + ' items and ' + str(longreads) + ' are longreads.')
+
+    elif options.info_choice == 'list':
+      # Retrieve info about the user's list
+      response = pt.get_list(consumer_key, settings.pocket_access_token)
+      items = response['list']
+      longreads = 0
+      for item in items:
+        # is it a long read?
+        if 'word_count' in items[item]:
+          words = int(items[item]['word_count'])
+          longread = True if  words > settings.longreads_wordcount else False
+        else:
+          longread = False
+        if longread:
+          longreads += 1
+      print('The user list has ' + str(len(response['list'])) + ' items and ' + str(longreads) + ' are longreads.')
 
   # purge options
-  if hasattr(options, 'purge_choice'):
+  elif hasattr(options, 'purge_choice'):
 
     if options.purge_choice == 'list':
       print('\033[0;36mPurging tags in the list\033[0;m')
       purge = pt.purge_tags('unread', settings.retain_tags, archive_tag, consumer_key, settings.pocket_access_token)
       print(purge)
 
-    if options.purge_choice == 'archive':
+    elif options.purge_choice == 'archive':
       print('\033[0;36mPurging tags in the archive\033[0;m')
       purge = pt.purge_tags('archive', settings.retain_tags, archive_tag, consumer_key, settings.pocket_access_token)
       print(purge)
 
-    if options.purge_choice == 'all':
+    elif options.purge_choice == 'all':
       print('\033[0;36mPurging tags in both the archive and the list\033[0;m')
       purge = pt.purge_tags('all', settings.retain_tags, archive_tag, consumer_key, settings.pocket_access_token)
       print(purge)
 
-  if options.refresh:
+  elif options.refresh:
     print('Refreshing at ' + datetime.now().strftime('%a %d %b %Y %H:%M'))
     refresh = pt.refresh(*refresh_settings)
     print('\033[0;36m' + refresh + '\033[0;m')
 
-  if options.stash:
+  elif options.stash:
     stash = pt.stash(consumer_key, settings.pocket_access_token, archive_tag, settings.replace_all_tags, settings.retain_tags, settings.ignore_faves, settings.ignore_tags)
     print('\033[0;36m' + stash + '\033[0;m')
 
-  if options.test:
+  elif options.test:
     result = pt.test(consumer_key, settings.pocket_access_token)
     print(result)
   
